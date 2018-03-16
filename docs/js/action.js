31e08b729165f13d6b6df88423dbc1510295c96f
ICCU <31857877+omeu@users.noreply.github.com>
2018-03-16 12:04:49 +0800

    Update action.js

--- docs/js/action.js
+++ docs/js/action.js
@@ -1,11 +1,24 @@
-let btnNodejs = document.getElementById('btn-nodejs');
-let btnLinux = document.getElementById('btn-linux');
-let btnWeb = document.getElementById('btn-web');
+let btnJob = document.getElementById('btn-job');
+let btnClassName = null;
+let btnText = null;
 
-btnNodejs.addEventListener('click', removeBtn);
-btnLinux.addEventListener('click', removeBtn);
-btnWeb.addEventListener('click', removeBtn);
+btnJob.addEventListener('click', chageText);
 
-function removeBtn(e) {
+function chageText(e) {
+  btnClassName = e.target.className;
+  btnText = e.target.innerText;
   e.target.className = "d-none";
+  let changeInput = document.querySelector('#change-job');
+  changeInput.className = "input-group";
+  let input = changeInput.querySelector('input');
+  input.focus();
+  input.addEventListener('blur', changeJob);
+}
+
+function changeJob(e) {
+  let jobValue = e.target.value;
+  e.target.value = '';
+  btnJob.innerText = jobValue ? jobValue : btnText;
+  btnJob.className = btnClassName;
+  document.querySelector('#change-job').className = "d-none";
 }
